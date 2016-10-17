19d2752bd8feb59e5481e15aad892e0f05604a36
Omar Sucapuca <suca@Omars-MacBook-Pro.local>
2016-10-17 15:49:54 +0100

    Renaming Build folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     connect = require('gulp-connect');
 
-var builtFolder = 'www';
+var builtFolder = 'www-mobile';
 
 gulp.task('connect', function () {
   connect.server({
